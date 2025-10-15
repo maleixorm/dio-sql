c679178cff8bd837a49f9de363bb4f1a635ca258
Marcos Aleixo <maknytz@gmail.com>
2025-10-15 15:24:49 -0300

    Filtrando as seleções dos registros

--- select.sql
+++ select.sql
@@ -1,3 +1,6 @@
 SELECT * FROM viagens.usuarios;
 SELECT * FROM viagens.destinos;
 SELECT * FROM viagens.reservas;
+
+SELECT nome FROM destinos WHERE id = 7 OR id = 2;
+SELECT * FROM usuarios WHERE nome LIKE "%C%";
